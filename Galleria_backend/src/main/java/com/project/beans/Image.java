34ad532b2b8273fffbc78741419477f6f8268e2d
chrandle <christianrandle042@gmail.com>
2020-05-12 11:22:28 -0500

    JDBC connection fixes

--- Galleria_backend/src/main/java/com/project/beans/Image.java
+++ Galleria_backend/src/main/java/com/project/beans/Image.java
@@ -6,7 +6,7 @@ import javax.persistence.Table;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;;
+import javax.persistence.GenerationType;
 
 import com.project.beans.*;
 import com.project.repos.*;
